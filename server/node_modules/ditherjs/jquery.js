88bfd2fe32edc8c524733688c7dbb87792b01cae
1euro7cent <thomasruhl@live.de>
2021-05-20 09:45:58 +0200

    removed node moduled directory

--- server/node_modules/ditherjs/jquery.js
+++
@@ -1,9 +0,0 @@
-var DitherJS = (function( $ ) {
-    $.fn.ditherJS = function(options) {
-        var DitherJS = require('./lib/client');
-        var ditherjs = new DitherJS(options);
-        ditherjs.dither(this.selector);
-    };
-}( jQuery ));
-
-module.exports = DitherJS;
